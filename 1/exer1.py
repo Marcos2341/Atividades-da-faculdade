5fb180350f08739d98bde5b92d489083b75df7c0
MARCOS RUY <127743791+Marcos2341@users.noreply.github.com>
2023-03-17 20:10:44 +0000

    Exercicio 1 certo

--- 1/exer1.py
+++ 1/exer1.py
@@ -7,7 +7,8 @@ if aux%9 == 0:
   
     aux = sum(int (i) for i in str(aux))
     if aux == 9:
-          print("igual a 9")
+      a = a + 1
+      print("igual a 9")
     else:
       a = a + 1
       aux = sum(int (i) for i in str(aux))
